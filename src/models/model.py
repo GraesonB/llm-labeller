db2cc72f1d60f6028e4e2b53d796a1584b519174
Graeson Bergen <graesonbergen@gmail.com>
2024-08-02 18:16:08 -0600

    chore: added abc model class

--- src/models/model.py
+++ src/models/model.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import time
+from rich import print
+from aiohttp import ClientSession
 
 
 class Model(ABC):
@@ -24,9 +27,29 @@ class Model(ABC):
         pass
 
     @abstractmethod
-    async def label(self):
+    def format_body(self, prompt: str) -> dict:
         pass
 
     @abstractmethod
-    async def get_token_cost(self):
+    def get_token_cost(self, model_output: dict) -> dict:
         pass
+
+    @abstractmethod
+    def parse_output_text(self, model_output: dict) -> dict:
+        pass
+
+    async def label(self, prompt: str, prompt_params: dict) -> str:
+        formatted_prompt = prompt.format(**prompt_params)
+        body = self.format_body(formatted_prompt)
+
+        async with ClientSession(headers=self.headers) as session:
+            start = time.time()
+            res = await session.post(self.url, json=body)
+            res_json = await res.json()
+            end = time.time()
+
+        text = self.parse_output_text(res_json)
+        token_usage = self.get_token_cost(res_json)
+        seconds = end - start
+
+        return text, token_usage, seconds
