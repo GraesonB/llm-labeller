47e73f5dabc459014b4fe65debe9c77d4da005a4
Graeson Bergen <graesonbergen@gmail.com>
2024-08-02 20:07:02 -0600

    refactor: now iterating through models with for loop

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import asyncio
+from models.anthropic import ClaudeHaiku
 from models.deepseek import DeepSeek
 from models.google import Gemini
 from models.openai import GPT4oMini
@@ -12,26 +13,18 @@ if __name__ == "__main__":
         gemini = Gemini()
         deepseek = DeepSeek()
         gpt4o = GPT4oMini()
+        claude = ClaudeHaiku()
         # sentence = "하교에 가고 있어"
         sentence = "9점 이상을 쏘면 승리가 확정되는 상황, 김우진은 깨끗한 10점으로 금메달을 명중시켰습니다."
 
-        output, usage, seconds = await gemini.label(prompt, {"sentence": sentence})
-        print("GEMINI:")
-        print(output)
-        print(usage)
-        print(seconds)
-        print("")
-        output, usage, seconds = await deepseek.label(prompt, {"sentence": sentence})
-        print("DeepSeek:")
-        print(output)
-        print(usage)
-        print(seconds)
-        print("")
-        output, usage, seconds = await gpt4o.label(prompt, {"sentence": sentence})
-        print("GPT 4o:")
-        print(output)
-        print(usage)
-        print(seconds)
-        print("")
+        for model in [gemini, deepseek, gpt4o, claude]:
+            print(f"Model: {model}")
+            output, usage, seconds = await gemini.label(prompt, {"sentence": sentence})
+            print("Output:")
+            print(output)
+            print("Usage metrics:")
+            print(usage)
+            print(f"Seconds Taken: {seconds}")
+            print("")
 
     asyncio.run(main())
