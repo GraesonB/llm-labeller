93e466cd2cc0de6716ad5de49233c6ac6b9318de
Graeson Bergen <graesonbergen@gmail.com>
2024-08-04 14:56:04 -0600

    chore: removed unnecessary files from package

--- src/llm_labeller/constants.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv(override=True)
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
-ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
