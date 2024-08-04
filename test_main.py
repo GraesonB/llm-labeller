1d8d05e88fc60e2cddcf6698dc17c1d69ff8f01a
Graeson Bergen <graesonbergen@gmail.com>
2024-08-04 13:46:00 -0600

    chore: removed test file

--- test_main.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture
-def sample_data():
-    return {"key1": "value1", "key2": "value2"}
-
-def test_sample_data(sample_data):
-    assert sample_data["key1"] == "value1"
-    assert sample_data["key2"] == "value2"
