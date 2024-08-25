927e1c5d82632d315c2c5f7f51403906e28e26d8
Graeson Bergen <graesonbergen@gmail.com>
2024-08-25 17:23:47 -0600

    fix: added gemini tuned to init

--- src/llm_labeller/models/__init__.py
+++ src/llm_labeller/models/__init__.py
@@ -1,6 +1,6 @@
 from .anthropic import Claude
 from .deepseek import DeepSeek
 from .openai import OpenAI
-from .google import Gemini, GeminiGCP
+from .google import Gemini, GeminiGCP, GeminiTuned
 
-__all__ = ["Claude", "DeepSeek", "OpenAI", "Gemini", "GeminiGCP"]
+__all__ = ["Claude", "DeepSeek", "OpenAI", "Gemini", "GeminiGCP", "GeminiTuned"]
